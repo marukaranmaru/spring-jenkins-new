8bcb0e6d19c4b810eef141242b979a6987e557a7
marukaran <marukaran@gmail.com>
2023-07-16 16:01:54 -0700

    first

---
+++ src/main/java/com/example/demo/SpringJenkinsNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsNewApplication.class, args);
+	}
+
+}
