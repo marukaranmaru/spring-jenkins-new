f4c2d6ff7b5c0a60de2ea9a14e2306e6375c27ce
marukaran <marukaran@gmail.com>
2023-07-16 16:16:49 -0700

    commiting

--- src/main/java/com/example/demo/SpringJenkinsNewApplication.java
+++ src/main/java/com/example/demo/SpringJenkinsNewApplication.java
@@ -1,12 +1,24 @@
 package com.example.demo;
 
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringJenkinsNewApplication {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsNewApplication.class);
+
+	@PostConstruct
+	public void init()
+	{
+		logger.info("Application started ... ");
+	}
 	public static void main(String[] args) {
+		logger.info("Application executed ... ");
 		SpringApplication.run(SpringJenkinsNewApplication.class, args);
 	}
 
--- src/test/java/com/example/demo/SpringJenkinsNewApplicationTests.java
+++ src/test/java/com/example/demo/SpringJenkinsNewApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringJenkinsNewApplicationTests {
 
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsNewApplicationTests.class);
 	@Test
 	void contextLoads() {
+		logger.info("Test case executing....");
+		assertEquals(true , true);
 	}
 
 }
